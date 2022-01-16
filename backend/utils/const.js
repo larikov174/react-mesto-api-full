eea4b4948216930201a70b71fff359ec3b24cbcf
Andrey Larikov <larikov174@gmail.com>
2022-01-16 16:59:27 +0500

    fix: edit regExp

--- backend/utils/const.js
+++ backend/utils/const.js
@@ -1,4 +1,4 @@
 // module.exports.regExp = /(\b(https?|ftp|file):\/\/)?[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]/;
 // module.exports.db = 'mongodb+srv://larikov174:2694432@cluster0.mwtfk.mongodb.net/mestodb';
-module.exports.regExp = /(https?:\/\/(www\.)?)[\w-]+\.[\w./():,-]+/gi;
+module.exports.regExp = /(https?:\/\/(www\.)?)[\w-]+\.[\w./():,-]+/i;
 module.exports.db = 'mongodb://localhost:27017/mestodb';
